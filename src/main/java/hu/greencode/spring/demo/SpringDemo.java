360e3a931100de2591e7e45bcb1fcb565e86bbe0
Sandor Vasi <sandor.vasi@corballis.ie>
2015-04-13 00:02:11 +0200

    Todos bean has been added in order to demonstrate scopes

--- src/main/java/hu/greencode/spring/demo/SpringDemo.java
+++ src/main/java/hu/greencode/spring/demo/SpringDemo.java
@@ -1,17 +1,30 @@
 package hu.greencode.spring.demo;
 
+import hu.greencode.spring.demo.todo.Todo;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan
+@ComponentScan(basePackages = {"hu.greencode.spring.demo"})
 public class SpringDemo {
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDemo.class, args);
     }
 
+    @Bean(name = "todos")
+//    @Scope(value = "singleton", proxyMode = ScopedProxyMode.TARGET_CLASS)
+    @Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
+//    @Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public Map<String, Todo> todos() {
+        return new HashMap<String, Todo>();
+    }
 }
